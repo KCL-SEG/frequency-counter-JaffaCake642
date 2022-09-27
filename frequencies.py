f8ef19dfe9694d38ed7b5ce8444c0d4b5cb9628b
Abbas BinVakas <abbas.bin_vakas@kcl.ac.uk>
2022-09-27 21:39:21 +0100

    Completed task

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,12 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    newList = []
+    for item in items:
+        newList.append(str(item))
+    items = newList
+    frequencies = {x:items.count(x) for x in items}
+
     return frequencies
